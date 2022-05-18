fd6d7d12a2531a0a0766ccb949afaa69cb89b09f
ktitaro <hello@ktitarov.com>
2022-05-18 04:25:34 +0300

    Measured compression %

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,59 @@
 use std::io::prelude::*;
 use wasm_bindgen::prelude::*;
+use flate2::read::{GzDecoder, ZlibDecoder, DeflateDecoder};
+use flate2::write::{GzEncoder, ZlibEncoder, DeflateEncoder};
 use flate2::Compression;
-use flate2::read::{GzEncoder, ZlibEncoder, DeflateEncoder};
-use flate2::write::{GzDecoder, ZlibDecoder, DeflateDecoder};
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn gzip_encode(data: &str) -> String {
-    let input = data.as_bytes();
-    let mut buffer = Vec::new();
-    let mut encoder = GzEncoder::new(input, Compression::default());
-    encoder.read_to_end(&mut buffer).unwrap();
-    String::from_utf8(buffer).unwrap()
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(data.as_bytes()).unwrap();
+    let compressed_bytes = encoder.finish().unwrap();
+    base64::encode(&compressed_bytes)
 }
 
 #[wasm_bindgen]
 pub fn gzip_decode(data: &str) -> String {
-   let input = data.as_bytes();
-   let mut buffer = Vec::new();
-   let mut decoder = GzDecoder::new(buffer);
-   decoder.write_all(&input).unwrap();
-   decoder.try_finish().unwrap();
-   buffer = decoder.finish().unwrap();
-   String::from_utf8(buffer).unwrap()
+    let compressed_bytes = base64::decode(&data).unwrap();
+    let mut decoder = GzDecoder::new(&compressed_bytes[..]);
+    let mut buffer = String::new();
+    decoder.read_to_string(&mut buffer).unwrap();
+    return buffer;
 }
 
 #[wasm_bindgen]
 pub fn zlib_encode(data: &str) -> String {
-    let input = data.as_bytes();
-    let mut buffer = Vec::new();
-    let mut encoder = ZlibEncoder::new(input, Compression::default());
-    encoder.read_to_end(&mut buffer).unwrap();
-    String::from_utf8(buffer).unwrap()
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(data.as_bytes()).unwrap();
+    let compressed_bytes = encoder.finish();
+    base64::encode(&compressed_bytes.unwrap())
 }
 
 #[wasm_bindgen]
 pub fn zlib_decode(data: &str) -> String {
-   let input = data.as_bytes();
-   let mut buffer = Vec::new();
-   let mut decoder = ZlibDecoder::new(buffer);
-   decoder.write_all(&input).unwrap();
-   decoder.try_finish().unwrap();
-   buffer = decoder.finish().unwrap();
-   String::from_utf8(buffer).unwrap()
+    let compressed_bytes = base64::decode(&data).unwrap();
+    let mut decoder = ZlibDecoder::new(&compressed_bytes[..]);
+    let mut buffer = String::new();
+    decoder.read_to_string(&mut buffer).unwrap();
+    return buffer;
 }
 
 #[wasm_bindgen]
 pub fn deflate_encode(data: &str) -> String {
-    let input = data.as_bytes();
-    let mut buffer = Vec::new();
-    let mut encoder = DeflateEncoder::new(input, Compression::default());
-    encoder.read_to_end(&mut buffer).unwrap();
-    String::from_utf8(buffer).unwrap()
+    let mut encoder = DeflateEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(data.as_bytes()).unwrap();
+    let compressed_bytes = encoder.finish().unwrap();
+    base64::encode(&compressed_bytes)
 }
 
 #[wasm_bindgen]
 pub fn deflate_decode(data: &str) -> String {
-   let input = data.as_bytes();
-   let mut buffer = Vec::new();
-   let mut decoder = DeflateDecoder::new(buffer);
-   decoder.write_all(&input).unwrap();
-   decoder.try_finish().unwrap();
-   buffer = decoder.finish().unwrap();
-   String::from_utf8(buffer).unwrap()
+    let compressed_bytes = base64::decode(&data).unwrap();
+    let mut decoder = DeflateDecoder::new(&compressed_bytes[..]);
+    let mut buffer = String::new();
+    decoder.read_to_string(&mut buffer).unwrap();
+    return buffer;
 }
